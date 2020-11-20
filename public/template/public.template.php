1a8d4a5f45a2bdcf741c79d19c14da7de56d3303
crhubhub <clo73877@gmmail.com>
2020-11-20 10:33:12 +0100

    update style

--- public/template/public.template.php
+++ public/template/public.template.php
@@ -28,9 +28,11 @@
     </div>
     <h1>Antiques</h1>
     <h1>Dealer</h1>
-    <div id="hook">
-        <h4>Trouvez la pièce d'exeption et de collection qui vous manque</h4>
-        <p>Voir les articles disponibles</p>
+    <div id="support">
+        <div>
+        <h4>"Trouvez la pièce d'<em class="italic-bold">exeption</em><br>et de <em class="italic-bold">collection</em> qui vous manque"</h4>
+            <p class="gold-txt"><a class="gold-txt" href="">Voir les articles disponibles <!--&#9670--></a></p>
+        </div>
     </div>
 
 </header>
@@ -46,5 +48,9 @@
 <footer>
     <br><br><a href="?page=login" style="">Log In</a>
 </footer>
+<script>
+
+</script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </body>
 </html>
