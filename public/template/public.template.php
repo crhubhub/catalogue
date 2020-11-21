8bb069fa12552ad1c33ed20fa2d1fe36b5991155
crhubhub <clo73877@gmmail.com>
2020-11-21 22:53:13 +0100

    more css - home nearly ok

--- public/template/public.template.php
+++ public/template/public.template.php
@@ -6,12 +6,20 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style/public.style.css">
+    <link rel="stylesheet" media="screen and (min-width: 1600px)" href="style/l-public.style.css"/>
+    <link rel="stylesheet" media="screen and (min-width: 2000px)" href="style/xl-public.style.css"/>
+    <link rel="stylesheet" media="screen and (max-width: 850px)" href="style/m-public.style.css"/>
+    <link rel="stylesheet" media="screen and (max-width: 700px)" href="style/s-public.style.css"/>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,600;0,800;1,600&display=swap"
           rel="stylesheet">
     <title><?= $pageTitle ?? 'Antiques Dealer' ?> (public)</title><!--(public) is to del-->
 </head>
 <body>
+<div id="background"></div>
+<h2>
+    <?= $h2 ?? ' ' ?>
+</h2>
 <header>
     <nav>
         <ul>
@@ -30,14 +38,15 @@
     <h1>Dealer</h1>
     <div id="support">
         <div>
-        <h4>"Trouvez la pièce d'<em class="italic-bold">exeption</em><br>et de <em class="italic-bold">collection</em> qui vous manque"</h4>
+            <h4>"Trouvez la pièce d'<em class="italic-bold">exeption</em><br>et de <em
+                        class="italic-bold">collection</em> qui vous manque"</h4>
             <p class="gold-txt"><a class="gold-txt" href="">Voir les articles disponibles <!--&#9670--></a></p>
         </div>
     </div>
 
 </header>
+
 <main>
-    <h2><?= $h2 ?? ' ' ?></h2>
     <?php
     if (isset($_GET['error'])) {
         echo $_GET['error'];
@@ -51,6 +60,7 @@
 <script>
 
 </script>
-<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"
+        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 </body>
 </html>
--- v/public-home.view.php
+++ v/public-home.view.php
@@ -1,6 +1,39 @@
-HOME
+<div class="wrapper">
+    <div class="h-left">
+        <div style="background: aliceblue" class="h-left-nogap">
+            <div style="background: cadetblue" id="h-news">News</div>
+            <div style="background: red" class="wrapper-nogap">
+                <div style="background: burlywood">promos</div>
+                <div style="background: darkgoldenrod">promos</div>
+            </div>
+        </div>
+        <div id="h-categories">Une div</div>
+    </div>
+    <div class="" id="h-right">
+        <div id="h-articles">ARTS</div>
+        <div id="h-shops">SHOPS</div>
+    </div>
+
+
+</div>
+
+<form action="" id="h-form" style="display: flex; flex-direction: column">
+    <h3>Contactez-nous</h3>
+    <div class="box-x">
+        <label for="name"></label>
+        <input type="text" id="name" name="name" placeholder="Votre nom">
+
+        <label for="email"></label>
+        <input type="email" id="email" name="email" placeholder="Votre email">
+    </div>
+    <label for="subject"></label>
+    <textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+    <div style="display: flex; justify-content: flex-end">
+        <input type="submit" value="Submit">
+    </div>
+</form>
 <?php
 $pageTitle = 'Accueil';
 $h2 = 'Bienvenue';
 $pageContent = ob_get_clean();
-require dirname(__DIR__).DIRECTORY_SEPARATOR.'public'.DIRECTORY_SEPARATOR.'template'.DIRECTORY_SEPARATOR.'public.template.php';
+require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'public' . DIRECTORY_SEPARATOR . 'template' . DIRECTORY_SEPARATOR . 'public.template.php';
